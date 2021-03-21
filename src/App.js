54fef943bcfb1342f6340402a198d0ef808a1cad
henokwel <hen.ok@live.com>
2021-03-21 23:01:49 +0100

    init files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [msg, setMsg] = useState("none")
+  useEffect(() => {
+
+    fetch("api/getTest")
+      .then(res => res.json())
+      .then(data => setMsg(data.msg))
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {msg}
       </header>
     </div>
   );
